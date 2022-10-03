621284a54bddab2defef034d374120516b61cd5d
Curtis Stallings <curtis.stallings@rockwellautomation.com>
2022-10-03 07:35:20 -0500

    readfile attempt

---
+++ readfile.py
@@ -0,0 +1,9 @@
+import sys
+def read(file_path):
+    with open(file_path, "r") as file:
+        file_str = str(file.read())
+        return file_str
+
+if __name__ == "__main__":
+    print(sys.arg[1])
+    read(sys.argv[1])
