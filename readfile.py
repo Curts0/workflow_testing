8c2663e5c18683fa8a7b31a39dd2ec56ec5b7311
Curtis Stallings <curtis.stallings@rockwellautomation.com>
2022-10-04 15:14:25 -0500

    reading directory

--- readfile.py
+++ readfile.py
@@ -4,6 +4,15 @@ def read(file_path):
         file_str = str(file.read())
         return file_str
 
+import os
+def tree_printer(root):
+    for root, dirs, files in os.walk(root):
+        for d in dirs:
+            print(os.path.join(root, d))
+        for f in files:
+            print(os.path.join(root, f))
+
 if __name__ == "__main__":
     print(sys.argv)
+    tree_printer('.')
     #read(sys.argv[1])
