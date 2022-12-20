ceb6d64870f1b5ceceb6ed00659d427e4a20eb15
Curtis Stallings <curtis.stallings@rockwellautomation.com>
2022-12-20 09:53:33 -0600

    test

--- install_bak.sql
+++ install_bak.sql
@@ -1,6 +1,6 @@
 USE master;
 GO
-RESTORE DATABASE [AdventureWorksDW2017] 
+RESTORE DATABASE [AdventureWorks2017] 
 FROM  DISK = 'D:\a\workflow_testing\workflow_testing\AdventureWorksDW2017.bak';
 GO
 --"D:\a\workflow_testing\workflow_testing\adventureworks\"
